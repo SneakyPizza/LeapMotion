e286b833b2aa3e4d45b1da796d53a7fe2989739f
MushroomToad <satori360@hotmail.com>
2014-05-27 12:12:37 +0200

    mouseInputTestCommit
    yolo

--- wereld draait door/Assets/Scripts/Player/PlayerMove.cs
+++ wereld draait door/Assets/Scripts/Player/PlayerMove.cs
@@ -4,19 +4,19 @@ using Leap;
 
 public class PlayerMove : MonoBehaviour {
 
-	public PlayerRotate playerRotate;
-	public float playerMoveSpeed;
-	public Vector3 playerDir;
+
 
 	void Update()
 	{
-		transform.rotation = playerRotate.playerRotation;
+
 	}
 
 	void FixedUpdate ()
 	{
-			playerDir *= Time.deltaTime;
-
+		if (Input.GetKey(KeyCode.A)) {
+			rigidbody.AddForce(1*Time.deltaTime,0,0);
+		}		
+			
 			// the bigger the player, speed up
 			//rigidbody.AddForce ( * playerMoveSpeed);
 	}
