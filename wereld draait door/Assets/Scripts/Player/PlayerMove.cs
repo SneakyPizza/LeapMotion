96a3a43b3f3b5433d2ef16d399fbfa8d8124dc60
MushroomToad <satori360@hotmail.com>
2014-05-28 12:22:50 +0200

    playerMoveMouseScript
    Fixes the script that makes sure you can use mouse input.

--- wereld draait door/Assets/Scripts/Player/PlayerMove.cs
+++ wereld draait door/Assets/Scripts/Player/PlayerMove.cs
@@ -1,21 +1,30 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using Leap;
 
 public class PlayerMove : MonoBehaviour {
 
-
+	public Vector3 mouseInput;
+	public Vector3 currentPosition;
+	public float mouseSpeed;
 
 	void Update()
 	{
 
+		mouseInput = new Vector3(0,0,-Input.GetAxis("Mouse X"));
+		currentPosition += mouseInput;
+		transform.position += currentPosition / mouseSpeed;
+		Debug.Log (currentPosition);
+		//transform.Translate.y = new Vector3(Input.GetAxis("Mouse X"), Input.GetAxisRaw("Mouse Y"), 0); 
 	}
 
 	void FixedUpdate ()
 	{
 		if (Input.GetKey(KeyCode.A)) {
-			rigidbody.AddForce(1*Time.deltaTime,0,0);
+			rigidbody.AddForce(0,0,20*Time.deltaTime);
+			Debug.Log("a");
 		}		
+
 			
 			// the bigger the player, speed up
 			//rigidbody.AddForce ( * playerMoveSpeed);
